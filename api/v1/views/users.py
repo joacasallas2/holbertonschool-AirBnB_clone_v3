fe6f8597d99eb28fbe0a79bfd2e518a4f662c69b
joana.casallas <joacasallas@gmail.com>
2025-02-11 12:02:38 -0500

    Restfull routes - users

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -7,7 +7,7 @@ from models import storage
 from models.user import User
 
 
-@app_views.route("/user", methods=["GET"],
+@app_views.route("/users", methods=["GET"],
                  strict_slashes=False)
 def get_users():
     """Retrieve all users"""
@@ -25,7 +25,7 @@ def get_user(user_id):
     return jsonify(user.to_dict())
 
 
-@app_views.route("/user/<string:user_id>", methods=["DELETE"],
+@app_views.route("/users/<string:user_id>", methods=["DELETE"],
                  strict_slashes=False)
 def delete_user(user_id):
     """Delete a user by its ID"""
@@ -58,7 +58,7 @@ def create_user():
     return jsonify(new_user.to_dict()), 201
 
 
-@app_views.route("/user/<string:user_id>", methods=["PUT"],
+@app_views.route("/users/<string:user_id>", methods=["PUT"],
                  strict_slashes=False)
 def update_user(user_id):
     """Update amenity"""
