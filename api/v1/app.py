7798f2650588325c46dedd7f75d7a476fd8d5395
joana.casallas <joacasallas@gmail.com>
2025-02-11 14:05:25 -0500

    cors

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,11 +3,13 @@
 """Flask App"""
 import os
 from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 
 
 app = Flask(__name__)
+CORS(app)
 
 
 app.register_blueprint(app_views)
