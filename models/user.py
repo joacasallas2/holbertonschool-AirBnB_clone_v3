35bf922baef9245b0b865ea14955e0331eca19bd
joana.casallas <joacasallas@gmail.com>
2025-02-12 10:47:11 -0500

    hash password

--- models/user.py
+++ models/user.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 """ holds class User"""
-import models
-from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
+import hashlib
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+import models
+from models.base_model import BaseModel, Base
 
 
 class User(BaseModel, Base):
@@ -13,17 +12,28 @@ class User(BaseModel, Base):
     if models.storage_t == 'db':
         __tablename__ = 'users'
         email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
+        _password = Column("password", String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
         places = relationship("Place", backref="user")
         reviews = relationship("Review", backref="user")
     else:
         email = ""
-        password = ""
+        _password = ""
         first_name = ""
         last_name = ""
 
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
+
+
+    @property
+    def password(self):
+        """Password getter - prevent direct access"""
+        return self._password
+
+    @password.setter
+    def password(self, value):
+        """Hashes password before setting"""
+        self._password = hashlib.md5(value.encode()).hexdigest()
