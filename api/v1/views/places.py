5f62c147cd4aebdffaa8fba5cd5ed944fa0d1593
joana.casallas <joacasallas@gmail.com>
2025-02-13 14:24:21 -0500

    16. Search

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -7,6 +7,8 @@ from models import storage
 from models.city import City
 from models.place import Place
 from models.user import User
+from models.state import State
+from models.amenity import Amenity
 
 
 @app_views.route("cities/<string:city_id>/places", methods=["GET"],
@@ -66,6 +68,45 @@ def create_place(city_id):
     storage.new(new_place)
     storage.save()
     return jsonify(new_place.to_dict()), 201
+
+
+@app_views.route("/places_search", methods=["POST"],
+                 strict_slashes=False)
+def all_places_id():
+    """Return list of places according the filter required"""
+    if not request.is_json:
+        abort(400, description="Not a JSON")
+    data = request.get_json()
+    if data is None:
+        return [place.to_dict() for place in storage.all(Place).values()]
+    list_ids = []
+    list_cities = []
+    list_places = []
+    for requirement in data:
+        for id_obj in data[requirement]:
+            list_ids.append(id_obj)
+        if requirement == "states":
+            all_states = storage.all(State)
+            for k, v in all_states.items():
+                if v.id in list_ids:
+                    for city in v.cities:
+                        list_cities.append(city)
+        elif requirement == "cities":
+            all_cities = storage.all(City)
+            for k, v in all_cities.items():
+                if v.id in list_ids:
+                    list_cities.append(v)
+        elif requirement == "amenities":
+            all_places = storage.all(Place)
+            for place in all_places.items():
+                for k, v in place.amenities:
+                    if v.id in list_ids:
+                        list_places.append(v.to_dict())
+    for city in list_cities:
+        for place in city.places:
+            if place not in list_places:
+                list_places.append(place.to_dict())
+    return jsonify(list_places), 201
 
 
 @app_views.route("/places/<string:place_id>", methods=["PUT"],
